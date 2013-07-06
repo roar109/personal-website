69ad0ea8e1072124396a673361e4897ec5d61115
Hector Mendoza <roar109@gmail.com>
2013-07-05 19:53:31 -0500

    Add 4040,500 error handler

--- website.rb
+++ website.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'haml'
 
 set :bind=>'0.0.0.0'
-enable :logging
+disable :logging
 
 get '/' do
 	@activeTab='root'
@@ -13,4 +13,13 @@ end
 get '/about' do
 	@activeTab='about'
 	haml :about
+end
+
+not_found do
+  haml :'404'
+end
+
+error do
+  @e = request.env['sinatra_error']
+  haml :'500'
 end
